508b061f4a4ed78226346e8cfd9fde71d049218c
Ywanony <tryshyamsundar@gmail.com>
2026-09-24 06:01:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ from supabase import Client, create_client
 # CONFIGURATION
 # ============================================================
 
-BOT_TOKEN = "8879890822:AAFmg2uoxXDVCL1KA7BVNuSuyqqXVWojkow"
+BOT_TOKEN = "8879890822:AAE89FNDBwNWrr1RbDVFwPau_Oqr_cKE3i0"
 
 ADMIN_IDS = [
     8863002910,
@@ -34,7 +34,7 @@ ADMIN_IDS = [
 ]
 
 SUPPORT_USERNAME = "VlPSuppot"
-PUBLIC_DEMO_CHANNEL_LINK = "https://t.me/AII_Videos_groups"
+PUBLIC_DEMO_CHANNEL_LINK = "https://t.me/AII_Videos_group"
 
 # Supabase Credentials
 SUPABASE_URL = "https://rzpowbdtzbeivhxkkern.supabase.co"
